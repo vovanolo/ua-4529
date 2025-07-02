083d74f830f07a7a89eb312d7689c60faa724458
Volodymyr I Pankiv <vpankiv@softserveinc.com>
2025-07-02 19:49:21 +0300

    lesson1

---
+++ lesson1.js
@@ -0,0 +1,223 @@
+// console.log("Hello, World!");
+
+// console.log("Hello, World!");
+// console.log("Hello, World!");
+// console.log("Hello, World!");
+
+
+// вивід в консоль hello
+// console.log("hello"); // вивід в консоль
+
+// коментар
+
+/* 
+писати
+великий
+коментар
+*/
+
+// змінні
+
+// let const var
+
+// let message = "Hello";
+
+// let message;
+
+// message = "Hello";
+
+// message = 5;
+
+// console.log(message);
+
+// const PI = 3.14;
+
+// PI = 4;
+
+// console.log(PI);
+
+// let messageFromUser
+
+// let message = "Hello";
+
+// console.log(message);
+// console.log(message);
+// console.log(message);
+// console.log(message);
+// console.log(message);
+
+
+
+// Number
+
+// 12 2424 4242 1.5 2.3
+
+// let number = 12;
+
+// String
+
+//  "text 242 fsafad " 'dgdgdgdcvcvfb'
+
+
+// Boolean
+
+// true false
+
+
+// Undefined
+
+// let message;
+
+// console.log(message);
+
+// null
+
+// let message = 2;
+
+// message = null;
+
+// Object
+
+// let user = {
+//     name: "John",
+//     age: 30,
+//     isAdmin: true
+// }
+
+// alert("Hello, World!");
+
+// prompt("Enter your name:");
+
+
+// let userName = prompt("Enter your name:");
+
+// console.log(userName);
+
+// alert("Hello " + userName + "!");
+
+// alert(`Hello ${userName}!`);
+
+// typeof
+
+// let message = "Hello";
+
+// let message = null;
+
+
+
+// console.log(typeof message); // "string"
+
+// operators
+
+let a = 10;
+let b = 4;
+
+// console.log(a / b); 
+
+// console.log(a % b); //остача від ділення
+
+// a /= 2;
+
+// a++;
+// a += 1;
+
+// a--;
+// a -= 1;
+// console.log(a); // 11
+// a = a + 2;
+
+// console.log(a ** 1/2);
+
+// console.log(Math.pow(a, 2));
+
+// console.log(Math.sqrt(a));
+
+
+// string
+
+// let m1 = "Hello";
+// let m2 = "World";
+
+// console.log(m1 + m2);
+
+// console.log("hello" + 5);
+
+// console.log("5" + 5);
+
+// console.log("5" + true);
+
+
+// console.log("10" + "5");
+
+// console.log("10" - 5); // 5
+
+
+// переведення типів даних
+
+// let str = "10.334";
+
+// console.log(+str); // 10
+
+// console.log(parseInt(str)); // 10
+
+// console.log(parseFloat(str)); // 10
+
+
+// let number = 10;
+
+// console.log(String(number)); // "10"
+
+// console.log(Boolean(number)); // "10"
+
+
+// 0 - false >0 - true
+
+// "" - false  "sf" - true
+
+
+
+
+// console.log(10 == 11);
+
+// console.log(10 != 11);
+
+// console.log(10 >= 5);
+
+// console.log(0 == "");
+
+// console.log(0 === 0);
+
+// ! - логіне НЕ
+
+// && - логічне І
+
+// || - логічне АБО
+
+// !true == false
+
+
+
+// let x = 25;
+
+
+// console.log(x > 0 && x < 20); 0-20;
+
+// console.log(x < 0 || x > 20); // або менше за 0 або більше за 20
+
+// тернарний оператор
+
+// let age = 10;
+
+// (age >= 18) ? console.log("Ви повнолітній") : console.log("Ви неповнолітній");
+
+// let age = prompt("Введіть ваш вік:");
+
+// let result = (age >= 18) ? "Ви повнолітній" : "Ви неповнолітній";
+
+// console.log(result);
+
+
+ 
+
+
+
